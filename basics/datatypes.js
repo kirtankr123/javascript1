50161b8390ceb4db8492a0bd49059ab6399b153c
kirtankr123 <kirtanraj8210@gmail.com>
2026-05-03 19:08:16 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,3 @@
+let name = "Kirtan"    // string
+let age = 20           // numeric
+let isStudent= true    // boolean
---
+++ basics/variables.js
@@ -0,0 +1,6 @@
+const id=1234
+let name="Kirtan"
+var accId="Y3j5k7cx"
+acccity="Gaya"
+
+console.table([id , name,accId,acccity]);
